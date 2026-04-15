0574e8688029ae0d69a4e61a9d9dbf4b4399bb54
Abdullah Shmaitelly <ashmaitelly@gmail.com>
2026-04-15 12:18:41 +0300

    Added missing hamburger icon

--- app/src/main/java/com/abdsh/studenthelper/MainActivity.java
+++ app/src/main/java/com/abdsh/studenthelper/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.navigation.NavigationView;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,18 +14,25 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
         final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, myToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
         NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-                drawerLayout.closeDrawers();
-                int id=menuItem.getItemId();
-                Intent  intent=new Intent();
+                int id = menuItem.getItemId();
+                Intent intent = null;
                 if (id == R.id.nav_notes) {
                     intent = new Intent(MainActivity.this, NotesActivity.class);
                 } else if (id == R.id.nav_tands) {
@@ -31,43 +40,30 @@ public class MainActivity extends AppCompatActivity {
                 } else if (id == R.id.nav_sched) {
                     intent = new Intent(MainActivity.this, ScheduleActivity.class);
                 }
-                startActivity(intent);
-                return false;
-                /* old implementation
-                if(id==R.id.nav_notes){
-                        Intent intent=new Intent(MainActivity.this,NotesActivity.class);
-                        startActivity(intent);}
-                else if(id==R.id.nav_tands){
-                        Intent intent1=new Intent(MainActivity.this,tands.class);
-                        startActivity(intent1);}
-                else if(id==R.id.nav_sched){
-                    Intent intent1=new Intent(MainActivity.this,ScheduleActivity.class);
-                    startActivity(intent1);}*/
 
-
-
+                if (intent != null) {
+                    startActivity(intent);
+                }
+                drawerLayout.closeDrawers();
+                return true;
             }
-
         });
-    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             greeting gf = new greeting();
             fragmentTransaction.replace(R.id.content_frame, gf);
-            fragmentTransaction.addToBackStack(null);
             fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
             fragmentTransaction.commit();}
-
   }
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        finish();
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
